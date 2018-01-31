f736290f43c1060614d27eda34fad90f76c23f35
J.Lyu <jlyu@wpi.edu>
2018-01-31 14:39:08 -0500

    Add connect()() and combineReducers() README

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
-
+// {
+// 	books: [{title: 'Harry Potter'},{title: 'JavaScript'}],	// Book Reducer
+// 	activeBook: {title: 'JavaScript: The Good Parts'} // ActiveBook Reducer
+// }
+// The mapping of state:
+// This reducer adds a key "books" to our global application state,
+// and the value is whatever gets returned from BooksReducer,
+// which in this case is an array of books.
 const rootReducer = combineReducers({
   books: BooksReducer
+  // anotherKey: AnotherReducer
 });
 
 export default rootReducer;
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,4 +1,4 @@
-export default function() {
+export default function() {	// return an array of books
 	return [
 		{ title: 'JavaScript: The Good Parts'},
 		{ title: 'Harry Potter'},
